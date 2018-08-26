3e1d8b3e79f7a13ed97014dbc50e50248e1addec
mnzit <mnzitshakya@gmail.com>
2018-08-26 21:18:59 +0545

    Closure in Lambda Expression e1

---
+++ src/Tuter2/ClosureExample.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tuter2;
+
+/**
+ *
+ * @author Dell
+ */
+public class ClosureExample {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20; 
+        doProcess(a, new Process() {
+            @Override
+            public void process(int i) {
+                System.out.println(i + b);
+            }
+
+        });
+    }
+
+    public static void doProcess(int i, Process p) {
+        p.process(i);
+    }
+}
+
+interface Process {
+
+    void process(int i);
+}
